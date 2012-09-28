60e287e81255b410be5191f18ea143a2f64f5eb6
walter huang <walter.i.huang@gmail.com>
2012-09-28 15:49:55 +0800

    first commit

---
+++ the_same_number.c
@@ -0,0 +1,60 @@
+﻿/**
+ * @file    the_same_number1.c
+ * @brief   ...
+ * @author  H034115
+ * @date    9/28/2012 3:36:15 PM
+ */
+/*
+  http://weibo.com/1915548291/yDjra5ajO#1348465113389
+  一个大小为n的数组，里面的数都属于范围[0, n-1]，有不确定的重复元素，
+  找到至少一个重复元素，要求O(1)空间和O(n)时间。
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+
+//swap will not success for value type.
+void swap(int x, int y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+//swap will success for refference type
+void swap_pointer(int *x, int *y)
+{
+    int temp; 
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int find_duplicate(int numberList[] , int n)
+{
+    int i , j;
+    for( i=0;i<n;i++)
+    {
+        while(numberList[i]!=i)
+        {
+          if(numberList[i]==numberList[numberList[i]])
+          {
+             return numberList[i];
+          }
+          else
+          {
+             //swap(numberList[i],numberList[numberList[i]]);   This will not swap this two variable.
+             swap_pointer(&numberList[i],&numberList[numberList[i]]);
+          }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int numberList[]= {3,5,7,4,8,3,5,2,9,1};
+    assert(3==find_duplicate(numberList,10));
+    return 0;
+}
---
+++ the_same_number_sample.c
@@ -0,0 +1,70 @@
+/**
+ * @file    the_same_number.c
+ * @brief   ...
+ * @author  H034115
+ * @date    9/28/2012 3:12:35 PM
+ */
+/*
+  http://weibo.com/1915548291/yDjra5ajO#1348465113389
+  一个大小为n的数组，里面的数都属于范围[0, n-1]，有不确定的重复元素，
+  找到至少一个重复元素，要求O(1)空间和O(n)时间。
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+
+#define NELEMS(a) (sizeof(a)/sizeof(a[0]))
+void swap(int *x, int *y)
+{
+  int tmp = *x; *x = *y; *y = tmp;
+}
+
+/*
+  Term: "right place": a[i] = i, "wrong place": a[i] != i
+  the while loop executes at most n times, because each execution
+  puts an element from the "wrong place" into the "right place",
+  and there are at most n elements at "wrong place".
+  So, total execution time is for(n) + while(n) = O(n)
+*/
+int find_duplicate(int a[], int n)
+{
+  int i;
+  for(i = 0; i < n; i++) {
+    /* a[0]..a[i-1] are in right place */
+    while(a[i] != i) {
+
+        printf("%d",a[i]);
+        printf("%s"," ");
+      if (a[i] == a[a[i]])
+	return a[i];
+      else
+	swap(&a[i],&a[a[i]]);
+    }
+  }
+  /* a[0]..a[n-1] are in right place
+     no duplicate one */
+  return -1;
+}
+
+void shuffle(int a[],int n)
+{
+  while(--n > 0) {
+    int k = rand() % (n+1);
+    swap(&a[k],&a[n]);
+  }
+}
+
+int main()
+{
+  /* simple test */
+  int i;
+  srand(clock());
+  for(i = 0; i < 10; i++) {
+    int a[] = {1,2,3,4,5,6,7,8,8};
+    shuffle(a,NELEMS(a));
+    assert(8 == find_duplicate(a,NELEMS(a)));
+  }
+  return 0;
+}
